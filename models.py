2c1543160bf22cebbe9637db2ccda276263f50df
staechap-bic <supachai.bic@gmail.com>
2026-04-18 19:14:39 +0700

    Add files via upload

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
-from .database import Base
+from database import Base
 from datetime import date, datetime
 
 class Category(Base):
